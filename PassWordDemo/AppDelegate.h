73afcb9a3a5d69c04003f06c94f8c3236874cd5e
孟凡 <DYA1227@163.com>
2017-10-11 16:14:08 +0800

    Initial Commit

---
+++ PassWordDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PassWordDemo
+//
+//  Created by mengdy on 17/10/11.
+//  Copyright © 2017年 mengdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PassWordDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PassWordDemo
+//
+//  Created by mengdy on 17/10/11.
+//  Copyright © 2017年 mengdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
